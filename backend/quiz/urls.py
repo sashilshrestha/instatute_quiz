67cd7db30f7a8e01dc14b5fabd54bf1b53f3cdfa
hasan chamok <hasan.chamok16@gmail.com>
2024-05-13 06:12:34 +1000

    Score:hasan

--- backend/quiz/urls.py
+++ backend/quiz/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path("subjects/",views.SubjectController.as_view(),name="subject"),
     path("questionSets/",views.QuestionSetController.as_view(),name="questionSet"),
     path("questionBanks/",views.QuestionBankController.as_view(),name="questionBank"),
-    path("distributeQuestions/<subjectId>/",views.QuestionDistributorController.as_view(),name="questionDistributor")
-]
+    path("distributeQuestions/<subjectId>/",views.QuestionDistributorController.as_view(),name="questionDistributor"),
+    path("userQuiz/",views.UserQuizController.as_view(),name="userQuiz"),
+    path("userScore/<questionBankId>/",views.UserScoreController.as_view(),name="userScore")
+] 
