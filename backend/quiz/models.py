07ecda213e537c1650ad6782ed82861ec6a6de1b
hasan chamok <hasan.chamok16@gmail.com>
2024-05-13 19:58:39 +1000

    Fix:Hasan

--- backend/quiz/models.py
+++ backend/quiz/models.py
@@ -29,14 +29,24 @@ class QuestionBank(Document):
     createdAt = DateTimeField(default=datetime.datetime.now)
     updatedAt = DateTimeField(default=datetime.datetime.now)
 
-    
 class UserQuiz(Document):
     _id = StringField(required=False)
-    # userId = ReferenceField(User,required=True)
-    subjectId = ReferenceField(Subject,required = True)
-    questionBankId = ReferenceField(QuestionBank,required=True)
+    userId = ReferenceField(User, required=True)
+    subjectId = ReferenceField(Subject, required=True)
+    questionBankId = ReferenceField(QuestionBank, required=True)
     totalScores = IntField(required=True)
     totalQuestions = IntField(required=True)
     createdAt = DateTimeField(default=datetime.datetime.now)
     updatedAt = DateTimeField(default=datetime.datetime.now)
+
+    
+# class UserQuiz(Document):
+#     _id = StringField(required=False)
+#     # userId = ReferenceField(User,required=True)
+#     subjectId = ReferenceField(Subject,required = True)
+#     questionBankId = ReferenceField(QuestionBank,required=True)
+#     totalScores = IntField(required=True)
+#     totalQuestions = IntField(required=True)
+#     createdAt = DateTimeField(default=datetime.datetime.now)
+#     updatedAt = DateTimeField(default=datetime.datetime.now)
  
